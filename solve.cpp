3cf90359740f03a10dfd32a34d768351442b976f
HsuShengHsien <t0987235673@gmail.com>
2016-04-04 20:07:27 +0800

    first

---
+++ solve.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Sudoku.cpp"
+
+using namespace std;
+
+
+int main()
+{	
+	Sudoku ss;
+	ss.readIn();
+	ss.solve();
+					
+	return 0;	
+}
---
+++ transform.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Sudoku.cpp"
+
+using namespace std;
+
+
+int main()
+{	
+	Sudoku ss;
+	ss.readIn();
+	ss.transform();
+					
+	return 0;	
+}
